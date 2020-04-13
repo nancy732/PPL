9d7120fd10edf71e8fd6d19317d5c81161471a83
nancy-1312 <nancyarora213@gmail.com>
2020-04-13 12:42:27 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
-var express = require("express")
-var mongoose = require("mongoose")
-var app = express()
-var bodyParser = require('body-parser')
-var router = require('./router')
-const cors = require('cors')
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+var bodyParser = require("body-parser");
+var router = require("./router");
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }))
- 
-app.use(bodyParser.json())
-mongoose.connect('mongodb://localhost:27017/testMongoose', { useNewUrlParser: true, useUnifiedTopology: true }).
-     catch(error => handleError(error))
- 
-console.log("we are on server.js")
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', router)
-app.use(express.static(__dirname + '/public'));
-app.listen("8081", () => {
-    console.log("8081")
-})
+app.use(bodyParser.json());
+mongoose
+  .connect(
+    "mongodb+srv://nancy1312:Nancy@1312@cluster0-texqj.mongodb.net/testMongoose?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .catch(error => handleError(error));
+
+console.log("we are on server.js");
+
+app.use("/", router);
+app.use(express.static(__dirname + "/public"));
+app.listen(process.env.PORT || 8081, () => {
+  console.log("8081");
+});
